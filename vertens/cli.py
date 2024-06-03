0146a3a1c0ab33f255717725ff7d65d219408de1
manhhavu <manhha@htoh.io>
2024-06-03 21:04:46 +0200

    Translate from input to output JSON file

--- vertens/cli.py
+++ vertens/cli.py
@@ -1,11 +1,13 @@
 import typer
 import json
 
+from pathlib import Path
 from typing_extensions import Annotated, List, Dict, Optional
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers.string import StrOutputParser
+from more_itertools import chunked
 
 app = typer.Typer()
 
@@ -14,7 +16,8 @@ model = ChatOpenAI(model="gpt-3.5-turbo-0125")
 system_message = """
 Your name is Verpens. You are an expert translator. Your job is to translate i18n messages for applications.\n
 You should only translate, not answer any questions.\n
-You will be given a list of message and you should return the correspondant translated ones, in order. For example:
+Message could contains placeholder values, which are bound in double brackets, like {{number}}. These values should be kept as-is but you can however translate the rest.
+You will be given a list of message and you should return the correspondant translated ones, in order. 
                                
 GIVEN:
 - <message 1>
@@ -27,25 +30,38 @@ ANSWER:
 The messages to translate to {language} are the following:
 """.strip()
 
-user_message = """
-{% for message in messages %}- {{message}}{% endfor %}
-""".strip()
-
 prompt_template = ChatPromptTemplate.from_messages(messages=[
     ('system', system_message),
     MessagesPlaceholder('messages')
 ])
 
-
 chain = prompt_template | model | StrOutputParser()
 
 @app.command()
-def translate(language: str):
+def translate(language: str, input: Path, output: Path):
+    with open(input, 'r') as file:
+        source = json.loads(file.read())
+    
+    translated: Dict[str, str] = {}
+    for batch in chunked(list(source.items())[:10], 100):
+        keys = [item[0] for item in batch]
+        ms = [item[1] for item in batch]
+        ts = run(language, ms)
+        for (k, v) in zip(keys, ts):
+            translated[k] = v
+    
+    with open(output, 'w') as file:
+        json.dump(translated, file, ensure_ascii=False, indent=2)
+
+        
+def run(language: str, messages: List[str]) -> List[str]:
     response = chain.invoke({
         'language': language, 
-        'messages': [HumanMessage(content='- Your information has been updated\n-For the protection of your privacy, this event will remain strictly confidential')]
+        'messages': [HumanMessage(content='\n'.join(f"- {msg}" for msg in messages))]
     })
-    print(response)
+    translated = [ line[1:].strip() for line in response.split("\n") ]
+    return translated
+    
 
 if __name__ == "__main__":
     app()
